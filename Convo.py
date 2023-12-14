735e710defc34fccc110a8d3f231dcda8cd699cb
Mr Adi <107009032+Mradi007@users.noreply.github.com>
2023-12-14 17:30:58 +0500

    Create Convo.py

---
+++ Convo.py
@@ -0,0 +1,17 @@
+import os, platform, time, sys
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+    print('\033[0;97m[•] \033[1;32mCONGRATULATIONS 64BIT SUCCESS')
+    from Project1 import auto_chat_messenger
+    auto_chat_messenger()
+ 
+elif bit == '32bit':
+    print('\033[0;97m[•] \033[1;32mCONGRATULATIONS 32BIT SUCCESS')
+    from Project2 import auto_chat_messenger
+    auto_chat_messenger()
